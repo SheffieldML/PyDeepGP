9cdf0f5dfe403914c8115e580ae07bf23b1ff353
Neil Lawrence <lawrennd@gmail.com>
2017-12-29 23:06:48 +0100

    Replace tabs with spaces

--- deepgp/layers/mrd.py
+++ deepgp/layers/mrd.py
@@ -110,7 +110,7 @@ class MRDLayer(Parameterized):
         self.uncertain_inputs = uncertain_inputs
         Y = self.Y if self.layer_lower is None else self.layer_lower.X
         assert isinstance(dim_down, list) or isinstance(dim_down, tuple)
-	assert isinstance(kernel, list) and len(kernel)==len(dim_down), "The number of kernels has to be equal to the number of input modalities!"
+        assert isinstance(kernel, list) and len(kernel)==len(dim_down), "The number of kernels has to be equal to the number of input modalities!"
         super(MRDLayer, self).__init__(name=name)
         self.mpi_comm, self.mpi_root = mpi_comm, mpi_root
 
